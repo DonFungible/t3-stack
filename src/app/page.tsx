3d960136b21081ee873ec493348389d2f7fb854e
Don <100505855+DonFungible@users.noreply.github.com>
2024-05-20 21:56:49 -0700

    add force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/051df0d6-df48-42ba-abe9-38da5057c0e9-59z307.webp",
   "https://utfs.io/f/c6339f12-b7a9-4871-ac5c-2133b72ad9f6-t7hzin.webp",
@@ -8,10 +12,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log({ posts });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
